f70a6fd20d6217314f1ef29b1f5c2ef367c85740
Oculux <nate.williamson.nz@gmail.com>
2024-09-24 21:56:57 +1200

    :art: extract createFileIfNotExists into its own method and add comments + logging

--- src/main/java/org/se306/App.java
+++ src/main/java/org/se306/App.java
@@ -4,11 +4,15 @@ import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.se306.domain.Task;
 import org.se306.utils.GraphParser;
+import org.slf4j.Logger;
 
 /** Hello world! */
 public class App {
+
+  private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(App.class);
+
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    LOGGER.info("Hello World!");
     Graph<Task, DefaultWeightedEdge> graph = GraphParser.dotToGraph("dot/Nodes_7_OutTree.dot");
     GraphParser.graphToDot(graph, "test/App.dot");
   }
--- src/main/java/org/se306/utils/GraphParser.java
+++ src/main/java/org/se306/utils/GraphParser.java
@@ -14,23 +14,30 @@ import org.jgrapht.nio.dot.DOTExporter;
 import org.jgrapht.nio.dot.DOTImporter;
 import org.se306.App;
 import org.se306.domain.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GraphParser {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(GraphParser.class);
 
   public static Graph<Task, DefaultWeightedEdge> dotToGraph(String dotFileUrl) {
     Graph<Task, DefaultWeightedEdge> graph = new SimpleGraph<>(DefaultWeightedEdge.class);
     DOTImporter<Task, DefaultWeightedEdge> importer = new DOTImporter<>();
 
+    // Set how to read vertex attributes
     importer.setVertexWithAttributesFactory(
         (id, attributes) -> {
           int weight = Integer.parseInt(attributes.get("Weight").getValue());
           return new Task(id, weight);
         });
 
+    // Read from file
     try (InputStream dotFileInputStream = App.class.getResourceAsStream(dotFileUrl)) {
       importer.importGraph(graph, dotFileInputStream);
     } catch (IOException e) {
-      e.printStackTrace();
+      LOGGER.error("Error reading dot file", e);
+      throw new RuntimeException(e);
     }
 
     return graph;
@@ -39,26 +46,32 @@ public class GraphParser {
   public static void graphToDot(Graph<Task, DefaultWeightedEdge> graph, String dotFileUrl) {
     DOTExporter<Task, DefaultWeightedEdge> exporter = new DOTExporter<>();
 
+    // Set how to write vertex attributes
     exporter.setVertexAttributeProvider(
         task -> {
           return Map.of("Weight", DefaultAttribute.createAttribute(task.getWeight()));
         });
 
-    // Create file if it does not exist
+    // Write to file
+    createFileIfNotExists(dotFileUrl);
+    try (OutputStream dotFileOutputStream = Files.newOutputStream(Paths.get(dotFileUrl))) {
+      exporter.exportGraph(graph, dotFileOutputStream);
+      dotFileOutputStream.flush();
+    } catch (IOException e) {
+      LOGGER.error("Error writing dot file", e);
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static void createFileIfNotExists(String dotFileUrl) {
     try {
       Files.createDirectories(Paths.get(dotFileUrl).getParent());
       if (!Files.exists(Paths.get(dotFileUrl))) {
         Files.createFile(Paths.get(dotFileUrl));
       }
     } catch (IOException e) {
-      e.printStackTrace();
-    }
-
-    try (OutputStream dotFileOutputStream = Files.newOutputStream(Paths.get(dotFileUrl))) {
-      exporter.exportGraph(graph, dotFileOutputStream);
-      dotFileOutputStream.flush();
-    } catch (IOException e) {
-      e.printStackTrace();
+      LOGGER.error("Error creating file", e);
+      throw new RuntimeException(e);
     }
   }
 }
