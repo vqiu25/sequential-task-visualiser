faa16a215711a3cd7c6f0fa51ce3094f2773d1d5
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2024-09-25 08:32:43 +1200

    feat: implement picocli functionality in App.java

--- src/main/java/org/se306/App.java
+++ src/main/java/org/se306/App.java
@@ -1,8 +1,25 @@
 package org.se306;
 
+import org.se306.utils.SchedulerCommand;
+import org.slf4j.Logger;
+import picocli.CommandLine;
+
 /** Hello world! */
 public class App {
+
+  private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(App.class);
+
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    SchedulerCommand command = new SchedulerCommand();
+    CommandLine cmdLine = new CommandLine(new SchedulerCommand());
+    if (cmdLine.execute(args) != 0) {
+      return;
+    }
+
+    LOGGER.info("Input file: {}", command.getInputFileName());
+    LOGGER.info("Processors: {}", command.getProcessors());
+    LOGGER.info("Cores: {}", command.getCores());
+    LOGGER.info("Output file: {}", command.getOutputFileName());
+    LOGGER.info("Visualise: {}", command.checkVisualise());
   }
 }
